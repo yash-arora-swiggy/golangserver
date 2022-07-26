cf1f4380a95681cceff0e0660430ff5f1ed03dad
Yash Arora <yash.arora1@MacBook-Pro.bbrouter>
2022-07-26 14:29:57 +0530

    Added ss

--- main.go
+++ main.go
@@ -7,28 +7,28 @@ import (
 )
 
 type mydata struct {
-	name string `json:"name"`
+	Name string `json:"name"`
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
-
 	w.Header().Set("Content-Type", "application/json")
-	data := mydata {
-		name: "Yash",
-	}
+	data := mydata { Name: "Yash",}
+
 	switch r.Method {
 	case "GET":
 		err := json.NewEncoder(w).Encode(data)
 		if err != nil {
 			fmt.Errorf("error in encoding")
 		}
+		fmt.Println(data)
 	case "POST":
 		dataposted := mydata{}
 		err := json.NewDecoder(r.Body).Decode(&dataposted)
 		if err != nil {
 			fmt.Errorf("Error in decoding")
 		}
+		fmt.Println(dataposted)
 		fmt.Fprint(w, dataposted)
 	default:
 		fmt.Fprint(w, "Sorry, only GET and POST methods are supported.")
@@ -37,6 +37,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fmt.Println("Starting Server")
 
-	http.HandleFunc("/getandpost",handler)
+	http.HandleFunc("/",handler)
 	http.ListenAndServe("localhost:8080", nil)
 }
